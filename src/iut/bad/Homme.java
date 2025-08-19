6d2d1a63cdeef19d2d3b50b0b0e6bb2a40a27626
milanfandresena <milanfandresena28@gmail.com>
2025-08-19 13:18:48 +0300

    éplacez les attributs de Homme/Femme pour les placer dans le parent

--- src/iut/bad/Homme.java
+++ src/iut/bad/Homme.java
@@ -1,5 +1,13 @@
 package iut.bad;
 
-public class Homme {
+public class Homme extends Humain {
 
+    public Homme(String nom, String prenom, int age) {
+        super(nom, prenom, age); 
+    }
+
+    public static void main(String[] args) {
+        Homme h = new Homme("Dupont", "Jean", 25);
+        h.details();
+    }
 }
